d8ab2de6a24613c8bde34edcd13959fab1731d79
Fabrizio Gigli <42776858+fabrizioGigli@users.noreply.github.com>
2025-01-29 11:44:03 +0000

    Trying to append on root div

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 //const ApplicationMod = require('./application')
 //import App from './App';
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 const provadiv = document.createElement("div");
 const provah1 = document.createElement("h1");
 provah1.innerHTML = "<h1>prova<h1>";
 provadiv.appendChild(provah1);
-document.body.appendChild(provadiv);
+document.getElementById("root").appendChild(provadiv);
 //root.appendChild(provadiv);
 // root.render(
 //     <ApplicationMod />
