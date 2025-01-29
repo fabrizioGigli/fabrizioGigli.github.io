3887c8aa2cd6ad6e4e5076b48d6399d30c76b441
Fabrizio Gigli <42776858+fabrizioGigli@users.noreply.github.com>
2025-01-29 15:20:46 +0000

    Trying some mod

--- federico/src/App.js
+++ federico/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Ho editato la pagina, dovrebbe cambiare anche sul main.
         </p>
