a7dec57e0c87b1e43929ae399be89ef1e0d07196
Fabrizio Gigli <42776858+fabrizioGigli@users.noreply.github.com>
2025-01-29 11:03:56 +0000

    Removed React tag

--- index.js
+++ index.js
@@ -4,7 +4,5 @@ const ApplicationMod = require('./application')
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <ApplicationMod />
-  </React.StrictMode>
 );
