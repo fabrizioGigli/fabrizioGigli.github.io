76dd5b5f66c459ef93bbb458f00652c8674aaf9e
Fabrizio Gigli <42776858+fabrizioGigli@users.noreply.github.com>
2025-07-01 08:09:38 +0000

    Media queries and new classes for header

--- mysite/src/header/header.js
+++ mysite/src/header/header.js
@@ -13,17 +13,15 @@ const Header = () => {
           style={{ marginLeft: "5%", width: "25%" }}
         />
         <span className="headSpan">
-          <div>
-            <p style={{ fontSize: 50, fontWeight: "bold" }}>Fabrizio Gigli</p>
+          <div className="headTitle">
+            <p>Fabrizio Gigli</p>
           </div>
-          <div>
-            <p style={{ fontSize: 25, fontWeight: "bold" }}>
-              Artista - Sviluppatore
-            </p>
+          <div className="headSubtitle">
+            <p>Artista - Sviluppatore</p>
           </div>
         </span>
       </div>
-      <div style={{ display: "flex", margin: "2%" }}>
+      <div className="headButtonContainer">
         <div className="headButton">Portfolio</div>
         <div className="headButton">Pirografia</div>
       </div>
